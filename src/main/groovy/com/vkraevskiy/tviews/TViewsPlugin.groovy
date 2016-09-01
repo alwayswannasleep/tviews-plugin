2661c60ab5aaf2234d9ac65bfebbec30f0fc712b
Vlad Kraevskiy <vladkraevskii@gmail.com>
2016-09-01 09:42:34 +0300

    fix

--- src/main/groovy/com/vkraevskiy/tviews/TViewsPlugin.groovy
+++ src/main/groovy/com/vkraevskiy/tviews/TViewsPlugin.groovy
@@ -8,5 +8,9 @@ class TViewsPlugin implements Plugin<Project> {
     @Override
     void apply(Project target) {
         target.tasks.create("prepareFontStringsXml", GenerateTask.class)
+
+        target.afterEvaluate({
+            target.getTasks().getByName("preBuild").dependsOn "prepareFontStringsXml"
+        })
     }
 }
